4cccfb0645d8b05edb193d0bb89774dca6ef3829
Akkash Kumar <akkash@hotmail.co.uk>
2017-02-23 21:06:25 +0000

    InputParsing

--- src/master/Cache.java
+++ src/master/Cache.java
@@ -2,15 +2,25 @@ package master;
 
 import java.util.HashSet;
 
-public class Cache
-{
-    int cacheID;
+public class Cache {
+	
+	int cacheID;
     int totalVolume;
     int volumeLeft;
+    HashSet<Client> clients = new HashSet<Client>();
+    HashSet<File> files = new HashSet<File>();	// They work out
+    HashSet<HeapElement> heapBits = new HashSet<HeapElement>();
+	
+	public Cache(int id, int s) {
+		this.cacheID = id;
+		this.totalVolume = s;
+		this.volumeLeft = s;
+		
+	}
+	
+	public void updateClients(Client c) {
+		this.clients.add(c);
+	}
+	
     
-    HashSet<Client> clients;
-    
-    HashSet<File> files;
-    
-    HashSet<HeapElement> heapBits;
 }
--- src/master/File.java
+++ src/master/File.java
@@ -3,11 +3,22 @@ package master;
 import java.util.HashMap;
 import java.util.HashSet;
 
-public class File
-{
+public class File {
+	int fileID;
     int size;
 
-    HashMap<Client, Integer> requestCnt;
-    
     HashSet<HeapElement> heapBits;
+    HashMap<Client, Integer> requestCnt = new HashMap<Client, Integer>();
+    
+    public File(int id, int s) {
+    	this.fileID = id;
+    	this.size = s;
+    	
+    }
+    
+    public void addRequest(Client c, int i) {
+    	this.requestCnt.put(c, i);
+    }
+    
+    
 }
